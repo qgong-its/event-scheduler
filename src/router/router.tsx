64f62256aea156952a5afb41de64795b70281c85
Qian Gong <gongqian.de@gmail.com>
2026-04-28 08:53:37 +0200

    fix(api): correct authorization header

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -42,7 +42,7 @@ export const router = createBrowserRouter([
         ),
       },
       {
-        path: 'user',
+        path: 'editor',
         element: (
           <ProtectedRoute>
             <EventEditorPage />
